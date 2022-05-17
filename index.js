94aedd3a1d1a69750bfa0383ecd2e29cd3789409
Rashid khan <rashidkhanbd57@gmail.com>
2022-05-17 09:40:41 +0600

    after windows

--- index.js
+++ index.js
@@ -24,15 +24,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const doctorsCollection = client.db('doctors-portal').collection('services');
+    const servicesCollection = client.db('doctors-portal').collection('services');
     const bookingCollection = client.db('doctors-portal').collection('booking');
 
 
     app.get('/services', async(req , res )=>{
       const query = {};
-        const cursor =  doctorsCollection.find(query);
+        const cursor =  servicesCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
+    })
+    // get booking 
+    app.get('/available', async(req , res)=>{
+      const date = req.query.date || 'May 17, 2022';
+      // step 1 : get all the services
+      const services = await servicesCollection.find().toArray();
+      // step 2 : get all booking of the day 
+      const query = {date : date};
+      const booking = await bookingCollection.find(query).toArray();
+      // step 3 : for each service find booking for that service
+      services.forEach(service=>{
+        const serviceBooking = booking.filter(b=>b.treatment === service.name);
+        const booked = serviceBooking.map(s => s.slot);
+        const available = service.slots.filter(s=>!booked.includes(s));
+        service.available = available;
+        
+      })
+      res.send(services);
     })
 
     // for booking 
